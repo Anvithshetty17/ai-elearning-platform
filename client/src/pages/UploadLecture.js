743b01b80ee34488f8f17305982157c942ce128f
Anvith shetty <161817374+Anvithshetty17@users.noreply.github.com>
2025-08-07 11:30:24 +0530

    Add UploadLecture component

---
+++ client/src/pages/UploadLecture.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const UploadLecture = () => {
+  return (
+    <div>
+      <h1>Upload Lecture</h1>
+      {/* Add your component logic here */}
+    </div>
+  );
+};
+
+export default UploadLecture;
